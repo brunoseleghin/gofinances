634e4e80703db258afde98738d1833e5c40bff7b
Bruno Seleghin <brunoseleghin@gmail.com>
2022-07-10 10:56:35 -0300

    add gofinances

---
+++ App.tsx
@@ -0,0 +1,62 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
+import theme from './src/global/styles/theme';
+import { Register } from './src/screens/Register';
+import { Dashboard } from './src/screens/Dashboard';
+
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+
+        await Font.loadAsync({
+          Poppins_400Regular,
+          Poppins_500Medium,
+          Poppins_700Bold
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View
+      onLayout={onLayoutRootView}
+      style={{
+        flex: 1
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <Register />
+      </ThemeProvider>
+    </View>
+  );
+}
