f4b58f0ac81eb1b0509b4b67e19ffea260b9d627
Bruno Seleghin <brunoseleghin@gmail.com>
2022-07-10 11:15:30 -0300

    Add component Input

---
+++ src/components/Form/Input/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TextInputProps } from 'react-native';
+import theme from '../../../global/styles/theme';
+
+import { Container } from './styles';
+
+type Props = TextInputProps;
+
+export function Input({ ...rest }: Props) {
+  return (
+    <Container
+      placeholderTextColor={theme.colors.text}
+      {...rest}
+    />
+  );
+}
