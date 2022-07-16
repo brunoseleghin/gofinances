2f38f75f509eb69889a0b7dc966220d9e5d2b5ee
Bruno Seleghin <brunoseleghin@gmail.com>
2022-07-16 13:06:39 -0300

    Add intl

--- App.tsx
+++ App.tsx
@@ -1,3 +1,6 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
 import React, { useCallback, useEffect, useState } from 'react';
 import { StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
